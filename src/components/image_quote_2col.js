85a8284c9b954d6018207377aae24062efb6b96a
Sameer Segal <sameer@artoo.in>
2020-07-12 17:40:04 +0530

    Fixed build error

--- src/components/image_quote_2col.js
+++ src/components/image_quote_2col.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import Container from './Container';
+import Container from './container';
 import Img from 'gatsby-image';
 
 const IQsWrapper = styled.div`
